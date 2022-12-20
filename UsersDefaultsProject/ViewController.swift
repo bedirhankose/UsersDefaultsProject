dbbdc987af2be5baa84e5f8e8bd0273d8d67ac00
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-20 22:37:56 +0100

    Users defaults created.

--- UsersDefaultsProject/ViewController.swift
+++ UsersDefaultsProject/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var NoteTextField: UITextField!
+    @IBOutlet weak var TimeTextField: UITextField!
+    
+    @IBOutlet weak var NoteLabel: UILabel!
+    @IBOutlet weak var TimeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func SaveButton(_ sender: Any) {
+        NoteLabel.text = "Work to do: \(NoteTextField.text!)"
+        TimeLabel.text = "When to do: \(TimeTextField.text!)"
+    }
+    @IBAction func DeleteButton(_ sender: Any) {
+    }
+    
 }
 
