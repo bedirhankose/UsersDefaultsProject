00123cb7f17e3cddd37a03cf922b3b7177089c60
Bedirhan Köse <bedirhan.kose05@gmail.com>
2022-12-21 00:01:19 +0100

    The data has been made recordable.

--- UsersDefaultsProject/ViewController.swift
+++ UsersDefaultsProject/ViewController.swift
@@ -17,10 +17,26 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let savedNote = UserDefaults.standard.object(forKey: "Note")
+        let savedTime = UserDefaults.standard.object(forKey: "Time")
+        
+        // as -> casting
+        if let addedNote = savedNote as? String {
+            NoteLabel.text = "Work to do: \(addedNote)"
+        }
+        
+        if let addedTime = savedTime as? String {
+            TimeLabel.text = "When to do: \(addedTime)"
+        }
+        
     }
 
     @IBAction func SaveButton(_ sender: Any) {
+        
+        UserDefaults.standard.set(NoteTextField.text!, forKey: "Note")
+        UserDefaults.standard.set(TimeTextField.text!, forKey: "Time")
+        
         NoteLabel.text = "Work to do: \(NoteTextField.text!)"
         TimeLabel.text = "When to do: \(TimeTextField.text!)"
     }
